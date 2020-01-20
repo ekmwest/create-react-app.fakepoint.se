c4655f339a418f79a1e028b3113f329f70c5594b
Karl West <mickel.west@gmail.com>
2020-01-20 10:58:21 +0100

    css files

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import Comment from './Comment';
+import './Post.css';
 
 function Post({ post, users }) {
     return (
-        <>
-            <div className="post">
-                <div className="content">
-                    {post.content}
-                </div>
+        <div className="post">
+            <div className="content">
+                {post.content}
             </div>
             {post.comments.map(comment => (<Comment key={comment.id} comment={comment} users={users} />))}
-        </>
+        </div>
     );
 }
 
