3dcbc7656ad71fca425540220d0f01b737c9907d
Karl West <mickel.west@gmail.com>
2020-01-21 09:48:18 +0100

    css

--- src/Components/Comment/Comment.js
+++ src/Components/Comment/Comment.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import User from '../User/User'
 import styles from './Comment.module.css';
+import fmt from '../../Code/fmt';
 
 function Comment({ comment, users }) {
     return (
         <div className={styles.comment}>
             <div className={styles.header}>
                 <User user={users.find(user => user.id === comment.user_id)} />
+                <div className={styles.created_at}>{fmt.date(comment.created_at)}</div>
             </div>
             <div className={styles.content}>
                 {comment.content}
