175f72a6449d2952d5920472720ecf81b22aa3de
Dhanfia <dhanfia.rahman@viaecole.se>
2020-01-22 10:20:24 +0100

    Enter to post comment

--- src/Components/Comment/AddComment.js
+++ src/Components/Comment/AddComment.js
@@ -3,16 +3,19 @@ import styles from './AddComment.module.css';
 import useForm from './../../Hooks/useForm';
 import css from './../Button.module.css';
 
-function AddComment({ postId, saveComment }) {
-    const [values, changeHandler] = useForm({ content: "", postId });
+function AddComment({ saveComment, postId }) {
+    const [values, setValues, changeHandler] = useForm({ content: "", postId });
+    const keyDownHandler = event => {
+        if (event.key === "Enter" && !event.shiftKey) {
+            saveComment(values);
+            setValues({ content: "", postId });
+        }
+    }
 
     return (
         <div className={styles.add_comment}>
             <div className={styles.comment_textarea}>
-                <textarea name="content" value={values.content} onChange={changeHandler} className={styles.content_input} />
-            </div>
-            <div>
-                <button className={css.button} onClick={() => saveComment(values)}>Add</button>
+                <textarea name="content" value={values.content} onChange={changeHandler} className={styles.content_input} onKeyDown={keyDownHandler} />
             </div>
         </div>
     );
