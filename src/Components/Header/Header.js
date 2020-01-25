745312817921b3323073c3260843a177c62f98a6
Karl West <mickel.west@gmail.com>
2020-01-25 16:54:43 +0100

    sidebar toggler button

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,12 +5,14 @@ import { SidebarContext } from '../../Contexts/SidebarContext';
 function Header({ title }) {
     const { sidebarOpen, toggleSidebar } = useContext(SidebarContext);
 
-    const cssClass = () => sidebarOpen ? `${styles.header} ${styles.sidebar_open}` : `${styles.header}`
+    const cssClass = () => sidebarOpen ? `${styles.header} ${styles.sidebar_open}` : `${styles.header}`;
+
+    const buttonTitle = () => sidebarOpen ? 'Hide Menu' : 'Show Menu';
 
     return (
         <div className={cssClass()}>
-            <button onClick={toggleSidebar}>=</button>
-            {title}
+            <button onClick={toggleSidebar} title={buttonTitle()}>&equiv;</button>
+            <span className={styles.title}>{title}</span>
         </div>
     );
 }
