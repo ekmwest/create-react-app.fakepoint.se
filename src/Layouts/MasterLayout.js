b5205b5ca95fb76a2abd61093024d5c4f507e6f9
Karl West <mickel.west@gmail.com>
2020-01-25 03:19:35 +0100

    mobile design

--- src/Layouts/MasterLayout.js
+++ src/Layouts/MasterLayout.js
@@ -2,19 +2,22 @@ import React, { useContext } from 'react';
 import Sidebar from '../Components/Sidebar/Sidebar';
 import styles from './MasterLayout.module.css'
 import { SidebarContext } from '../Contexts/SidebarContext';
+import { isMobile } from '../Code/utils';
 
 function Master({ children }) {
     const { sidebarOpen } = useContext(SidebarContext);
 
-    const cssClass = () => sidebarOpen ? `${styles.main} ${styles.sidebar_open}` : `${styles.main}`
+    const cssClass = () => sidebarOpen ? `${styles.main} ${styles.sidebar_open}` : `${styles.main}`;
+
+    const variantCssClass = () => isMobile() ? 'is-mobile' : 'is-desktop';
 
     return (
-        <>
+        <div className={variantCssClass()}>
             <Sidebar></Sidebar>
             <main className={cssClass()}>
                 {children}
             </main>
-        </>
+        </div >
     )
 };
 
