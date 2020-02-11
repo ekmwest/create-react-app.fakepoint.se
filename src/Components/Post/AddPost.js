910a10d75513e33b05bd0aab937d2eff8c0a284a
Dhanfia <dhanfia.rahman@viaecole.se>
2020-02-11 10:29:06 +0100

    Add post design update

--- src/Components/Post/AddPost.js
+++ src/Components/Post/AddPost.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,  { useState} from 'react';
 import styles from './AddPost.module.css';
 import useForm from './../../Hooks/useForm';
 import User from '../User/User';
@@ -6,21 +6,25 @@ import css from './../Button.module.css';
 
 function AddPost({ saveHandler, users }) {
     const [values, setValues, changeHandler] = useForm({ content: "" });
+    const [textareaOpen, setTextareaOpen] = useState(false);
+
     const onSaveHandler = event => {
         saveHandler(values);
         setValues({ content: "" });
+        setTextareaOpen(!textareaOpen);
     }
 
     return (
-        <div className={styles.add_post}>
+        <div className={textareaOpen ? `${styles.add_post} ${styles.open}` : `${styles.add_post}`}>
             <div className={styles.post_content}>
                 <User user={users.find(user => user.id === 4)} hideUserName="true" />
                 <div className={styles.post_textarea}>
-                    <textarea placeholder="Add a post..." name="content" value={values.content} onChange={changeHandler} className={styles.content_input} />
+                    <textarea placeholder="Skriv nyhet..." name="content" value={values.content} onChange={changeHandler} className={styles.content_input} onClick={() => setTextareaOpen(!textareaOpen)} />
                 </div>
             </div>
-            <div className={styles.post_send}>
-                <button className={css.button} onClick={onSaveHandler}>Add</button>
+            <div className={styles.post_options}>
+                <button className={css.button} onClick={() => setTextareaOpen(!textareaOpen)}>Avbryt</button>
+                <button className={[css.button, css.PRIMARY].join(' ')}  onClick={onSaveHandler}>Spara</button>
             </div>
         </div>
     );
