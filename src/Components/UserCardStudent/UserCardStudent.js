15291762c1f78dc0dee479153b8e897eb97a935f
Dhanfia <dhanfia.rahman@viaecole.se>
2020-08-05 09:06:31 +0200

    Image slide in chat

--- src/Components/UserCardStudent/UserCardStudent.js
+++ src/Components/UserCardStudent/UserCardStudent.js
@@ -1,32 +1,31 @@
-import React from 'react';
+import React,  { useState } from 'react';
 import styles from './UserCardStudent.module.css';
 import { Link } from 'react-router-dom';
 
 function UserCardStudent({ open, handleClose, user }) {
 
-    const userCardCssClass = () => open ? `${styles.usercard}  ${styles.open}` : `${styles.usercard}`;
-    
-    // const [userCardImageSlide, setUserCardImageSlide] = useState(false);
-    // const userCardImageSlideCssClass = () => userCardImageSlide ? `${styles.user_images}  ${styles.slide}` : `${styles.user_images}`;
+    const userCardCssClass = () => open ? `${styles.usercard}  ${styles.open}` : `${styles.usercard}`;    
+    const [userCardImageSlide, setUserCardImageSlide] = useState(false);
+    const userCardImageSlideCssClass = () => userCardImageSlide ? `${styles.user_images}  ${styles.slide}` : `${styles.user_images}`;
 
     return (
         <div className={userCardCssClass()}>
             <div className={styles.close} onClick={handleClose}> <img src="/icons/close.png" alt="close"></img></div>
-            <div className={styles.user_images}>
-                <div className={styles.avatar_image}>
+            <div className={userCardImageSlideCssClass()}>
+                <div className={styles.avatar_image} onClick={() => setUserCardImageSlide(!userCardImageSlide)}>
                     <img src={user.image_url} alt="User"></img>
-                    {/* <div className={styles.img_title}>Profilbild</div> */}
+                    <div className={styles.img_title}>Profilbild</div> 
                 </div>
-                {/* <div className={styles.school_image} onClick={() => setUserCardImageSlide(!userCardImageSlide)}>
-                    <img src="/icons/user-b.jpg" alt="User"></img>
-                    <div  className={styles.img_title}>Skolfoto</div>
-                </div> */}
+                <div className={styles.school_image} onClick={() => setUserCardImageSlide(!userCardImageSlide)}>
+                    <img src={user.image_url} alt="User"></img>
+                    <div className={styles.img_title}>Skolfoto</div>
+                </div> 
             </div>
             <div className={styles.details}>
                 <div className={styles.full_name}>{user.display_name}</div>
                 <div className={styles.meta}>{user.email}</div>
                 <div className={styles.meta}>{user.telephone}</div>
-                <Link className={styles.link_to_profile} to={`/profile`}>Min profil</Link>
+                <Link className={styles.link_to_profile} to={`/profile`}>Gå till profil</Link>
             </div>
         </div>
     );
